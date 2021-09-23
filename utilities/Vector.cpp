741c88606e3bdf90c8607fb9d9e1958ae513a88a
Janis Rohde <janisrohde@t-online.de>
2021-09-23 20:27:00 +0200

    Initial commit

---
+++ utilities/Vector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Janis Rohde on 23.09.21.
+//
+
+#include "Vector.h"
---
+++ utilities/Vector.h
@@ -0,0 +1,14 @@
+//
+// Created by Janis Rohde on 23.09.21.
+//
+
+#ifndef LIGHTFE_VECTOR_H
+#define LIGHTFE_VECTOR_H
+
+
+class Vector {
+
+};
+
+
+#endif //LIGHTFE_VECTOR_H
